6ac0d983533e751d884905e04e47ca53a0d92523
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-24 00:54:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,9 +29,8 @@ function iLoveTheBeatles(n) {
   var array = [];
   do {
     n = array.push('I love the Beatles!');
-    n++
-  } while (n <= 15); {
+    n++;
+  } while (n < 15); 
     
   }
-  return n;
-}
+  return array;
