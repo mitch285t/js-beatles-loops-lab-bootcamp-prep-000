0857d47dc25a497390e01733542204809296601d
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-24 00:55:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ JLfacts = [
     return FACTS
   }
 
-function iLoveTheBeatles(n) {
-  var array = [];
-  do {
-    n = array.push('I love the Beatles!');
-    n++;
-  } while (n < 15); 
-    
-  }
-  return array;
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
+}
